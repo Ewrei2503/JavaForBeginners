416222077647fbd2969e6114947faafc547fdc4b
Ewrei2503 <egor.kulesh.02@mail.ru>
2024-10-02 21:21:55 +0300

    1.Added Enum Sector.java 2.Changed LocalDateTime to ZonedDateTime 3.Added UUID type instead of Ticket.id String type

--- src/main/java/by/yahor_kulesh/model/Ticket.java
+++ src/main/java/by/yahor_kulesh/model/Ticket.java
@@ -1,29 +1,31 @@
 package by.yahor_kulesh.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 public class Ticket {
-    private String ID;
+    private final UUID id = UUID.randomUUID();
+    private final ZonedDateTime ticketCreationTime = ZonedDateTime.now();
+
     private String concertHall;
     private String eventCode;
-    private LocalDateTime date;
+    private ZonedDateTime date;
     private boolean isPromo;
-    private char sector;
+    private Sector sector;
     private double backpackWeight;
-    private final LocalDateTime ticketCreationTime = LocalDateTime.now();
     private BigDecimal price;
 
     public Ticket() {}
 
-    public Ticket(String concertHall, String eventCode, LocalDateTime date) {
+    public Ticket(String concertHall, String eventCode, ZonedDateTime date) {
         this.concertHall = concertHall;
         this.eventCode = eventCode;
         this.date = date;
     }
 
-    public Ticket(String ID, String concertHall, String eventCode, LocalDateTime date, boolean isPromo, char sector, double backpackWeight, BigDecimal price) {
-        this.ID = ID;
+    public Ticket(String concertHall, String eventCode, ZonedDateTime date, boolean isPromo, Sector sector, double backpackWeight, BigDecimal price) {
         this.concertHall = concertHall;
         this.eventCode = eventCode;
         this.date = date;
@@ -33,8 +35,8 @@ public class Ticket {
         this.price = price;
     }
 
-    public String getID() {
-        return ID;
+    public UUID getId() {
+        return id;
     }
 
     public String getConcertHall() {
@@ -45,7 +47,7 @@ public class Ticket {
         return eventCode;
     }
 
-    public LocalDateTime getDate() {
+    public ZonedDateTime getDate() {
         return date;
     }
 
@@ -53,7 +55,7 @@ public class Ticket {
         return isPromo;
     }
 
-    public char getSector() {
+    public Sector getSector() {
         return sector;
     }
 
@@ -61,7 +63,7 @@ public class Ticket {
         return backpackWeight;
     }
 
-    public LocalDateTime getTicketCreationTime() {
+    public ZonedDateTime getTicketCreationTime() {
         return ticketCreationTime;
     }
 
@@ -72,14 +74,14 @@ public class Ticket {
     @Override
     public String toString() {
         return "Ticket Info:\n" +
-                "ID: " + this.getID() +
+                "ID: " + this.getId() +
                 ";\nConcert Hall: " + this.getConcertHall() +
                 ";\nEvent Code: " + this.getEventCode() +
-                ";\nDate: " + (this.getDate() == null? null: this.getDate()) +
+                ";\nDate: " + (this.getDate() == null? null: this.getDate().format(DateTimeFormatter.RFC_1123_DATE_TIME)) +
                 ";\nPromo ticket: " + this.isPromo() +
                 ";\nSector: " + this.getSector() +
                 ";\nBackpack weight allowed: " + this.getBackpackWeight() +
-                ";\nWas bought: " + this.getTicketCreationTime() +
+                ";\nWas bought: " + (this.getTicketCreationTime() == null? null: this.getTicketCreationTime().format(DateTimeFormatter.RFC_1123_DATE_TIME)) +
                 ";\nPrice: " + (this.getPrice()==null?0.0:this.getPrice()) +
                 "$.\n\n\n";
     }
