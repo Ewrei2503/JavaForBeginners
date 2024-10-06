cfa61e8b2af4fea8b593099474070ddca1a870de
Ewrei2503 <egor.kulesh.02@mail.ru>
2024-10-06 18:57:59 +0300

    Created inputString() and inputBoolean() methods in InputValidator class

--- src/main/java/by/yahor_kulesh/validators/InputValidator.java
+++ src/main/java/by/yahor_kulesh/validators/InputValidator.java
@@ -1,5 +1,7 @@
 package by.yahor_kulesh.validators;
 
+import by.yahor_kulesh.exceptions.StringSizeException;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.InputMismatchException;
@@ -16,7 +18,7 @@ public class InputValidator {
             input = sc.nextInt();
         }
         catch(InputMismatchException e){
-            System.out.println("Not valid! Must contain integer number!");
+            System.err.println("Not valid! Must contain integer number!");
             sc.next();
             input = inputInt();
         }
@@ -28,13 +30,43 @@ public class InputValidator {
         try{
             d = sc.nextBigDecimal();
         } catch(InputMismatchException e){
-            System.out.println("Not valid! Must contain floating 'COMMA' number!");
+            System.err.println("Not valid! Must contain floating 'COMMA' number!");
             sc.next();
             d = inputBigDecimal(scale);
         }
         return d.setScale(scale, RoundingMode.HALF_UP);
     }
 
+    public String inputString(){
+        String input = sc.nextLine();
+        sc.next();
+        return input;
+    }
+
+    public String inputString(int size){
+        String input = sc.nextLine();
+        try{
+            if(input.length() > size){
+                throw new StringSizeException();
+            }
+        }catch(StringSizeException e){
+            System.err.println(e.getMessage());
+            input = inputString(size);
+        }
+        return input;
+    }
+
+    public boolean inputBoolean(){
+        int input = inputInt();
+        try{
+            if(input > 1 | input < 0){
+                throw new InputMismatchException("Must be 1(True) or 0(False)!");
+            }
+        } catch(InputMismatchException e){
+            System.err.println(e.getMessage());
+        }
+        return input == 1;
+    }
 
 
 }
