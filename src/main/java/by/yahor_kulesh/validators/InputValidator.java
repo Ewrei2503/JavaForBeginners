9ec737369ea0f159c01dece2a03a6eca5d704548
Ewrei2503 <egor.kulesh.02@mail.ru>
2024-10-06 20:37:41 +0300

    Added date validation in InputValidator.java

--- src/main/java/by/yahor_kulesh/validators/InputValidator.java
+++ src/main/java/by/yahor_kulesh/validators/InputValidator.java
@@ -4,6 +4,9 @@ import by.yahor_kulesh.exceptions.StringSizeException;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -12,10 +15,11 @@ public class InputValidator {
 
     private static final Scanner sc = new Scanner(System.in);
 
-    public int inputInt(){
+    public static int inputInt(){
         int input;
         try{
             input = sc.nextInt();
+            sc.nextLine();
         }
         catch(InputMismatchException e){
             System.err.println("Not valid! Must contain integer number!");
@@ -25,10 +29,11 @@ public class InputValidator {
         return input;
     }
 
-    public BigDecimal inputBigDecimal(int scale) {
+    public static BigDecimal inputBigDecimal(int scale) {
         BigDecimal d;
         try{
             d = sc.nextBigDecimal();
+            sc.nextLine();
         } catch(InputMismatchException e){
             System.err.println("Not valid! Must contain floating 'COMMA' number!");
             sc.next();
@@ -37,13 +42,11 @@ public class InputValidator {
         return d.setScale(scale, RoundingMode.HALF_UP);
     }
 
-    public String inputString(){
-        String input = sc.nextLine();
-        sc.next();
-        return input;
+    public static String inputString(){
+        return sc.nextLine();
     }
 
-    public String inputString(int size){
+    public static String inputString(int size){
         String input = sc.nextLine();
         try{
             if(input.length() > size){
@@ -56,7 +59,7 @@ public class InputValidator {
         return input;
     }
 
-    public boolean inputBoolean(){
+    public static boolean inputBoolean(){
         int input = inputInt();
         try{
             if(input > 1 | input < 0){
@@ -64,8 +67,28 @@ public class InputValidator {
             }
         } catch(InputMismatchException e){
             System.err.println(e.getMessage());
+            sc.next();
+            return inputBoolean();
         }
         return input == 1;
+    }
+
+    public static LocalDateTime inputTime(){
+        String input = inputString();
+        LocalDateTime localDateTime;
+        try{
+            localDateTime = LocalDateTime.parse(input, DateTimeFormatter.ofPattern("yyyyMMddHHmm"));
+            if(localDateTime.isBefore(LocalDateTime.now())){
+                throw new InputMismatchException("You trying to buy ticket in the past!");
+            }
+        } catch(DateTimeParseException e){
+            System.err.println("Not valid! Write time of event in YYYYMMDDHHmm format!");
+            localDateTime = inputTime();
+        } catch(InputMismatchException e){
+            System.err.println(e.getMessage());
+            localDateTime = inputTime();
+        }
+        return localDateTime;
     }
 
 
