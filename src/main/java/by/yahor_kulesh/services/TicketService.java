8877a23b9f5deb5495fb1f4bf2d924197e428bc6
Ewrei2503 <egor.kulesh.02@mail.ru>
2024-10-14 17:13:55 +0300

    - refactored TicketService

--- src/main/java/by/yahor_kulesh/services/TicketService.java
+++ src/main/java/by/yahor_kulesh/services/TicketService.java
@@ -18,25 +18,8 @@ import java.time.ZoneId;
 
 public class TicketService extends Data{
 	public static void testTicketService() {
-		Client client = new Client();
-		client.getTicket(new BusTicket(567.89));
-		client.getTicket(new ConcertTicket("Concert",567,true, Sector.B.toString()));
-		client.getTicket(new Ticket(InputValidator.inputTime("202502030405").atZone(ZoneId.systemDefault()), BigDecimal.valueOf(1234.567)));
-		client.print();
-		Admin admin = new Admin();
-		admin.print();
-		Ticket ticket = new Ticket(InputValidator.inputTime("202501010101").atZone(ZoneId.systemDefault()), BigDecimal.valueOf(1234.123));
-		ticket.print();
-		ConcertTicket concert = new ConcertTicket(ticket,"Hall",123,false, Sector.A.toString());
-		concert.print();
-		BusTicket busTicket = new BusTicket(ticket,123.1237);
-		busTicket.print();
-		ticket.share("+375291234567");
-		busTicket.share("+375291234567");
-		concert.share("e.k.02@out.com");
-		concert.share("e.k.02@out.com", "+375292283213");
-		admin.checkTicket(new BusTicket());
-		admin.checkTicket(concert);
+        ConcertTicket concert = testTicket();
+        Client client = testUser(concert);
 
         try{
             testTicketRepo();
@@ -44,6 +27,33 @@ public class TicketService extends Data{
             System.err.println(e.getMessage());
         }
         testUserRepo(client);
+    }
+
+    private static ConcertTicket testTicket() {
+        Ticket ticket = new Ticket(InputValidator.inputTime("202501010101").atZone(ZoneId.systemDefault()), BigDecimal.valueOf(1234.123));
+        ticket.print();
+        ConcertTicket concert = new ConcertTicket(ticket,"Hall",123,false, Sector.A.toString());
+        concert.print();
+        BusTicket busTicket = new BusTicket(ticket,123.1237);
+        busTicket.print();
+        ticket.share("+375291234567");
+        busTicket.share("+375291234567");
+        concert.share("e.k.02@out.com");
+        concert.share("e.k.02@out.com", "+375292283213");
+        return concert;
+    }
+
+    private static Client testUser(ConcertTicket concert) {
+        Client client = new Client();
+        client.getTicket(new BusTicket(567.89));
+        client.getTicket(new ConcertTicket("Concert",567,true, Sector.B.toString()));
+        client.getTicket(new Ticket(InputValidator.inputTime("202502030405").atZone(ZoneId.systemDefault()), BigDecimal.valueOf(1234.567)));
+        client.print();
+        Admin admin = new Admin();
+        admin.print();
+        admin.checkTicket(new BusTicket());
+        admin.checkTicket(concert);
+        return client;
     }
 
     private static void testUserRepo(Client client) {
