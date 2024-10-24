49854519b82f9e156b296d254db6900800b43155
Ewrei2503 <egor.kulesh.02@mail.ru>
2024-10-24 17:47:04 +0300

    - add Hibernate init

---
+++ src/main/java/by/yahor_kulesh/dao/SessionFactoryProvider.java
@@ -0,0 +1,28 @@
+package by.yahor_kulesh.dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
+public class SessionFactoryProvider {
+
+    private static SessionFactory sessionFactory;
+
+    private SessionFactoryProvider() {}
+
+    public static SessionFactory getSessionFactory(Object obj) {
+        if (sessionFactory == null) {
+            try{
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(obj.getClass());
+                StandardServiceRegistryBuilder builder =
+                        new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+            } catch (Exception e) {
+                System.out.println("An error occurred during Hibernate init: " + e);
+            }
+        }
+        return sessionFactory;
+    }
+}
