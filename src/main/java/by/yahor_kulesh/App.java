a8e0387eda6d204b362befa93c6c3f2c0384394c
Ewrei2503 <egor.kulesh.02@mail.ru>
2024-11-07 18:45:18 +0300

    - removed unused inheritance

--- src/main/java/by/yahor_kulesh/App.java
+++ src/main/java/by/yahor_kulesh/App.java
@@ -3,11 +3,10 @@ package by.yahor_kulesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
